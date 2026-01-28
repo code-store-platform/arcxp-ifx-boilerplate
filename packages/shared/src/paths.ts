83fd6221d4befffe474add2e31dfdf9038fe5e0f
Max <msadovyi404@gmail.com>
2026-01-28 13:14:57 +0200

    create tmp in project root

--- packages/shared/src/paths.ts
+++ packages/shared/src/paths.ts
@@ -1,9 +1,15 @@
+import fs from "node:fs";
 import path from "node:path";
 
 const cwd = process.cwd();
 const definition = path.join(cwd, "ifx-definition.yaml");
 const root = path.resolve(__dirname, "../../../");
-const tmp = path.join(root, "tmp");
+const tmp = path.join(root, ".ifx-boiler-tmp");
+const is_project_root = fs.existsSync(path.join(cwd, "integrations"));
+
+if (!fs.existsSync(tmp) && is_project_root) {
+	fs.mkdirSync(tmp);
+}
 
 export const paths = {
 	root,
